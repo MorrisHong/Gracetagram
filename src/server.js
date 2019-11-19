8a01650103a1457b2e641c51e0604f62c0ba6301
gracelove <govlmo91@gmail.com>
2019-11-19 20:52:33 +0900

    그래프큐엘 typeDef, resolvers 설정

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
+/**
 const typeDefs = `
     type Query{
         hello: String!
@@ -16,6 +19,13 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers });
+*/
+
+
+const server = new GraphQLServer({ schema });
+
+
+server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
